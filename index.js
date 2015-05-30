dae848eccfea0705d900f5dae416063024518b6b
Justin Woo <moomoowoo@gmail.com>
2015-05-31 01:11:59 +0900

    rewrite crudely with rxjs to actually do shit async and keep shit reliable

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var Rx = require('rx');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 var chalk = require('chalk');
+
 var config = require('./config');
 
 var downloadDir = __dirname + '/downloads/'
-var downloadedFiles;
 
 function matchName(base, target) {
   return base.indexOf(target) !== -1;
@@ -20,84 +21,48 @@ function isBlacklisted(descriptor) {
   return false;
 }
 
-function isDownloaded(filename) {
-  for (var i = 0; i < downloadedFiles.length; i++) {
-    var downloadedFile = downloadedFiles[i];
-    if (matchName(downloadedFile, filename)) {
+function isDownloaded(fileDetails, filename) {
+  for (var i = 0; i < fileDetails.length; i++) {
+    var downloadedFilename = fileDetails[i].filename;
+    if (matchName(downloadedFilename, filename)) {
       return true;
     }
   }
   return false;
 }
 
-function handleJob(job) {
-  if (!isDownloaded(job.descriptor)) {
-    if (isBlacklisted(job.descriptor)) {
-      // dont download blacklisted crap
-      return;
-    }
-    console.log(chalk.yellow('downloading', job.descriptor));
-    var newLink = job.link.replace('view', 'download');
-    var filepath = downloadDir + job.descriptor + '.torrent';
-    var curl = spawn('curl', [newLink, '-o', filepath]);
-    curl.on('close', function () {
-      if (!isBlacklisted(job.descriptor)) {
-        console.log();
-        console.log(chalk.green('downloaded', filepath));
-      } else {
-        console.log();
-        console.log(chalk.red('blacklisted file', filepath, 'will not be opened.'));
-      }
-    });
-    return job.descriptor;
-  }
-}
+var downloadedFilesStream = new Rx.Subject();
+var fileDetailsStream = new Rx.Subject();
 
-function processOutput(output) {
-  var newDownloads = [];
-  console.log(chalk.green('beginning output processing'));
-  var lines = output.split('\n');
-  lines.reverse().forEach(function (a) {
-    if (a.length > 1) {
-      var job = JSON.parse(a);
-      var jobResult = handleJob(job);
-      if (jobResult) {
-        newDownloads.push(jobResult);
-      }
+console.log(chalk.green('reading previously downloaded files'));
+fs.readdir(downloadDir, function (err, files) {
+  downloadedFilesStream.onNext(files);
+});
+
+downloadedFilesStream.subscribe(function (files) {
+  console.log(chalk.green('getting downloaded files\' details'));
+  var fileStatsStream = new Rx.Subject();
+  files.map(function (file) {
+    fs.stat(downloadDir + file, function (err, stat) {
+      fileStatsStream.onNext({
+        filename: file,
+        time: stat.mtime.getTime()
+      });
+    });
+  });
+
+  var fileDetails = [];
+  fileStatsStream.subscribe(function (item) {
+    fileDetails.push(item);
+
+    if (fileDetails.length === files.length) {
+      fileDetailsStream.onNext(fileDetails);
     }
   });
-  console.log(chalk.green('processing finished'));
-  console.log();
-  if (newDownloads.length > 0) {
-    console.log(chalk.yellow('New Files downloaded:'));
-    newDownloads.forEach(function (a) {
-      console.log(a + '.torrent');
-    });
-    console.log(chalk.green('See', downloadDir));
-  } else {
-    console.log(chalk.yellow('Nothing new.'));
-  }
-  console.log();
-  console.log(chalk.yellow('Previous:'));
-  var j = 10;
-  for (var i = 1; i < j; i++) {
-    var filename = downloadedFiles[downloadedFiles.length - i];
-    switch (filename) {
-      case '.DS_Store':
-      case '.gitkeep':
-        j++;
-        break;
-      default:
-        console.log(filename);
-    }
-  }
-}
-
-downloadedFiles = fs.readdirSync(downloadDir);
-downloadedFiles.sort(function(a, b) {
-  return fs.statSync(downloadDir + a).mtime.getTime() > fs.statSync(downloadDir + b).mtime.getTime();
 });
+
 console.log(chalk.green('fetching URLs'));
+var jobsStream = new Rx.Subject();
 var output = '';
 var path = __dirname + '/phantom/getUrls.js';
 var getUrls = spawn('phantomjs', [path]);
@@ -106,5 +71,47 @@ getUrls.stdout.on('data', function (data) {
 });
 getUrls.on('close', function () {
   console.log(chalk.green('fetching finished'));
-  processOutput(output);
+  console.log(chalk.green('beginning output processing'));
+  var lines = output.split('\n');
+  lines.reverse().forEach(function (a) {
+    if (a.length > 1) {
+      var job = JSON.parse(a);
+      jobsStream.onNext(job);
+    }
+  });
+});
+
+var downloadJobStream = new Rx.Subject();
+
+Rx.Observable.combineLatest(
+  fileDetailsStream,
+  jobsStream,
+  function (fileDetails, job) {
+    return {
+      fileDetails: fileDetails,
+      job: job
+    };
+  }
+).subscribe(function (latest) {
+  var job = latest.job;
+  var fileDetails = latest.fileDetails;
+  if (!isDownloaded(fileDetails, job.descriptor)) {
+    if (!isBlacklisted(job.descriptor)) {
+      downloadJobStream.onNext(job);
+    }
+  }
+});
+
+downloadJobStream.subscribe(function (job) {
+  console.log(chalk.yellow('downloading', job.descriptor));
+  var newLink = job.link.replace('view', 'download');
+  var filepath = downloadDir + job.descriptor + '.torrent';
+  var curl = spawn('curl', [newLink, '-o', filepath]);
+  curl.on('close', function () {
+    if (!isBlacklisted(job.descriptor)) {
+      console.log(chalk.green('downloaded', filepath));
+    } else {
+      console.log(chalk.red('blacklisted file', filepath, 'will not be opened.'));
+    }
+  });
 });

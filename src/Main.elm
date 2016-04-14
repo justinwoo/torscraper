b458ecb52af5209ca897f3d18e0626943a86471e
Justin Woo <moomoowoo@gmail.com>
2016-04-14 21:05:39 +0300

    format the code

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,56 @@
-module Main where
+module Main (..) where
 
 import String
 import Json.Decode exposing (Value)
 
-type alias File = String
+
+type alias File =
+  String
+
+
 type alias Target =
-  {
-    name : File,
-    url : String
+  { name : File
+  , url : String
   }
-type alias BannedWords = List File
-type alias DownloadedFiles = List File
-type alias FetchedTargets = List Target
-type alias DownloadRequest = List Target
+
+
+type alias BannedWords =
+  List File
+
+
+type alias DownloadedFiles =
+  List File
+
+
+type alias FetchedTargets =
+  List Target
+
+
+type alias DownloadRequest =
+  List Target
+
 
 port bannedWords : BannedWords
 port downloadedFiles : DownloadedFiles
 port fetchedTargets : FetchedTargets
 port getDownloadsSignal : Signal Value
-
 processFile : BannedWords -> DownloadedFiles -> Target -> List Target -> List Target
 processFile bannedWords downloadedFiles target targets =
   let
-    name = target.name
-    blacklisted = isBlacklisted bannedWords name
-    downloaded = isDownloaded downloadedFiles name
+    name =
+      target.name
+
+    blacklisted =
+      isBlacklisted bannedWords name
+
+    downloaded =
+      isDownloaded downloadedFiles name
   in
     if blacklisted || downloaded then
-       targets
+      targets
     else
-       target :: targets
+      target :: targets
+
 
 getDownloadRequests : a -> DownloadRequest
 getDownloadRequests _ =
@@ -38,16 +59,20 @@ getDownloadRequests _ =
     []
     fetchedTargets
 
+
 port requestDownloadsSignal : Signal DownloadRequest
 port requestDownloadsSignal =
   Signal.map
     getDownloadRequests
     getDownloadsSignal
 
+
 isBlacklisted : BannedWords -> File -> Bool
 isBlacklisted bannedFiles file =
   List.any (\x -> String.contains x file) bannedFiles
 
+
 isDownloaded : DownloadedFiles -> File -> Bool
 isDownloaded downloadedFiles file =
   List.any (\x -> String.contains file x) downloadedFiles
+

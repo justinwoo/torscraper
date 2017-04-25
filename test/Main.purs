709f336dd514e2c3fc117d97a3beb9c75cc16ce5
justinwoo <moomoowoo@gmail.com>
2017-04-25 21:21:11 +0300

    get rid of basically unneeded regex

--- test/Main.purs
+++ test/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Main (isDownloaded, isBlacklisted, isProperName)
+import Main (isDownloaded, isBlacklisted)
 import Test.Unit (suite, test)
 import Test.Unit.Assert (assert)
 import Test.Unit.Console (TESTOUTPUT)
@@ -19,12 +19,6 @@ main :: forall e.
     )
     Unit
 main = runTest do
-  suite "isProperName" do
-    test "make sure it finds the proper name" do
-      assert "this should work" $ isProperName "[Du] Lelunallella - 01 [720p].mkv" == true
-      assert "should not work" $ isProperName "[Gamla] on - Batch [720p].mkv" == false
-      assert "should not work" $ isProperName "[Du] paljon (01-12) [720p].mkv" == false
-      assert "should not work" $ isProperName "[Fria] sisua [720p].mkv" == false
   suite "isBlacklisted" do
     test "make sure it blacklists shit" do
       assert "should not work" $ blacklisted "porkkanapiirakka" == false
